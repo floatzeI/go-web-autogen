5441f49cf454b319738bfc150e53d24f3a13dc18
beak <27046219+floatzeI@users.noreply.github.com>
2021-09-27 20:00:35 -0400

    add templates

---
+++ templates/autogen_web/autogen_web.go
@@ -0,0 +1,15 @@
+package autogen_web
+
+import (
+	"github.com/gofiber/fiber/v2"
+	// [ImportRegistrations]
+)
+
+type AutoGenRegister struct {
+}
+
+// [FunctionRegistrations]
+
+func (a *AutoGenRegister) Run(app *fiber.App) {
+	// [StartupRegistrations]
+}
