b7a1521ab7b30590a82c6dbac8d7b4df07c32d5e
beak <27046219+floatzeI@users.noreply.github.com>
2021-09-28 17:02:33 -0400

    add support for struct controller system (still mostly unfinished; todo: di)

--- main.go
+++ main.go
@@ -34,136 +34,192 @@ func main() {
 	var numberRegex = regexp.MustCompile("[0-9]")
 	var packageNameRegex = regexp.MustCompile("package ([a-zA-Z_]+)")
 	var controllerMethods = make([]string, 0)
-	var controllerMethodDefinitiions = make([]string, 0)
+	var controllerMethodDefinitions = make([]string, 0)
 
+	// targets
 	target := "examples/project-one/"
-	targetFile := target + "controllers/example.go"
-	var rawFile, readErr = os.ReadFile(targetFile)
-	if readErr != nil {
-		log.Fatal(readErr)
+	controllersFolder := target + "/controllers"
+	// controllers
+	controllers, e := os.ReadDir(controllersFolder)
+	if e != nil {
+		panic(e)
 	}
-	var rawFileStr = string(rawFile)
-	var packageName = packageNameRegex.FindAllStringSubmatch(rawFileStr, -1)[0][1]
-	fset := token.NewFileSet()
-	node, err := parser.ParseFile(fset, "", rawFile, parser.ParseComments)
-	if err != nil {
-		log.Fatal(err)
-	}
-	for _, f := range node.Decls {
-		var autogenComments = make([]string, 0)
-		// var autogenBody = make([]string, 0)
+	for _, entry := range controllers {
+		targetFile := target + "controllers/" + entry.Name()
 
-		fn, ok := f.(*ast.FuncDecl)
-		if !ok {
-			continue
+		var rawFile, readErr = os.ReadFile(targetFile)
+		if readErr != nil {
+			log.Fatal(readErr)
 		}
-		var parametersList = make([]string, 0)
-		var comments = fn.Doc.Text()
-		var functionCalls = decoratorparser.GetFunctions(comments)
-		var _requiredCalls = functionCalls.GetCallsByName("Required")
-		var requiredParams = make([]string, 0)
-		for _, item := range _requiredCalls {
-			for _, p := range item.Arguments {
-				requiredParams = append(requiredParams, p)
-			}
+		var rawFileStr = string(rawFile)
+		var packageName = packageNameRegex.FindAllStringSubmatch(rawFileStr, -1)[0][1]
+		fset := token.NewFileSet()
+		node, err := parser.ParseFile(fset, "", rawFile, parser.ParseComments)
+		if err != nil {
+			log.Fatal(err)
 		}
-		// add defaults
-		autogenComments = append(autogenComments, "@Produce json")
-		var httpMethod = decoratorparser.ParseMethod(functionCalls)
-		var stringHttpMethod = decoratorparser.StringifyMethod(httpMethod)
-		autogenComments = append(autogenComments, stringHttpMethod.Comment)
-		var possibleResponses = decoratorparser.ParseResponse(functionCalls)
+		var baseUrl = ""
+		var apiControllerName = ""
+		var apiController *decoratorparser.FunctionEntry = nil
+		var foundConstructor = false
+		for _, f := range node.Decls {
 
-		var hasSuccess = false
-		for _, resp := range possibleResponses {
-			if resp.StatusCode >= 200 && resp.StatusCode <= 299 {
-				hasSuccess = true
-			}
-			if resp.Model == "" {
+			fn, ok := f.(*ast.FuncDecl)
+			if !ok {
+				gen, ok := f.(*ast.GenDecl)
+				if ok {
+					for _, spec := range gen.Specs {
+						switch spec.(type) {
+						case *ast.TypeSpec:
+							typeSpec := spec.(*ast.TypeSpec)
+							// todo this might cause issues. see: https://github.com/golang/go/issues/27477
+							parsedComments := decoratorparser.New(gen.Doc.List[0].Text, typeSpec.Name.Name, "constructor")
+							decoratorFunctions := parsedComments.GetFunctions()
+							if apiController != nil {
+								log.Fatal("Found 2+ controllers in ", targetFile, " : ", typeSpec.Name.Name, ", ", apiControllerName)
+							}
+							apiController = decoratorFunctions.GetFirstCallByName("Controller")
+							if apiController == nil {
+								fmt.Print("[info] Struct", typeSpec.Name.Name, "is not a controller - skipping")
+								continue
+							}
+							apiControllerName = typeSpec.Name.Name
+							baseUrl = apiController.Arguments[0]
+							fmt.Println("[info] controller baseUrl =", baseUrl)
+							if !strings.HasSuffix(baseUrl, "/") {
+								baseUrl = baseUrl + "/"
+							}
+
+						}
+					}
+				}
 				continue
 			}
-			asString := decoratorparser.StringifyResponse(resp)
-			autogenComments = append(autogenComments, asString)
-		}
-		var returns decoratorparser.ReturnType
-		if !hasSuccess {
-			var returnType = fn.Type.Results
-			if returnType == nil {
+			if !foundConstructor && fn.Name.Name == "New"+apiControllerName {
+				foundConstructor = true
+				fmt.Println("[info] Found constructor. Name=" + fn.Name.Name)
+				continue
+			}
+			// todo: add di
+			var parametersList = make([]string, 0)
+			var comments = fn.Doc.Text()
+			var parser = decoratorparser.New(comments, entry.Name(), fn.Name.Name)
+			var functionCalls = parser.GetFunctions()
+			var _requiredCalls = functionCalls.GetCallsByName("Required")
+			var requiredParams = make([]string, 0)
+			for _, item := range _requiredCalls {
+				for _, p := range item.Arguments {
+					requiredParams = append(requiredParams, p)
+				}
+			}
 
-			} else {
-				// return type for basic type: "int"
-				// return type for struct: "&{usersmodels GetUserById}"
-				// return type for struct slice: ""
-				switch xv := returnType.List[0].Type.(type) {
-				case *ast.ArrayType:
-					{
-						var arrayType = xv.Elt
-						var isSlice = xv.Len == nil
-						// get the type
-						var elementResult = decoratorparser.ParseReturnType(fmt.Sprintf("%v", arrayType))
-						if isSlice {
-							elementResult.Prefix = "[]"
-							returns = elementResult
-						} else {
-							// todo
-							panic("Array support is not yet implemented")
+			var httpMethod = parser.ParseMethod()
+			if !httpMethod.IsControllerAction {
+				continue
+			}
+			var stringHttpMethod = decoratorparser.StringifyMethod(httpMethod)
+
+			var possibleResponses = parser.ParseResponse()
+
+			var hasSuccess = false
+
+			var autogenComments = make([]string, 0)
+			autogenComments = append(autogenComments, "@Produce json")
+			autogenComments = append(autogenComments, stringHttpMethod.Comment) // add method
+			for _, resp := range possibleResponses {
+				if resp.StatusCode >= 200 && resp.StatusCode <= 299 {
+					hasSuccess = true
+				}
+				if resp.Model == "" {
+					continue
+				}
+				asString := decoratorparser.StringifyResponse(resp)
+				autogenComments = append(autogenComments, asString)
+			}
+			var returns decoratorparser.ReturnType
+			if !hasSuccess {
+				var returnType = fn.Type.Results
+				if returnType == nil {
+
+				} else {
+					// return type for basic type: "int"
+					// return type for struct: "&{usersmodels GetUserById}"
+					// return type for struct slice: ""
+					switch xv := returnType.List[0].Type.(type) {
+					case *ast.ArrayType:
+						{
+							var arrayType = xv.Elt
+							var isSlice = xv.Len == nil
+							// get the type
+							var elementResult = decoratorparser.ParseReturnType(fmt.Sprintf("%v", arrayType))
+							if isSlice {
+								elementResult.Prefix = "[]"
+								returns = elementResult
+							} else {
+								// todo
+								panic("Array support is not yet implemented")
+							}
+							break
 						}
-						break
+					default:
+						var str = fmt.Sprintf("%v", returnType.List[0].Type)
+						returns = decoratorparser.ParseReturnType(str)
 					}
-				default:
-					var str = fmt.Sprintf("%v", returnType.List[0].Type)
-					returns = decoratorparser.ParseReturnType(str)
 				}
-			}
 
-			var respToAdd = decoratorparser.StringifyResponse(decoratorparser.ResponseEntry{
-				StatusCode: 200,
-				Model:      returns.Prefix + "" + returns.ImportedPackage + "." + returns.Type,
-			})
-			autogenComments = append(autogenComments, respToAdd)
-		}
-		fmt.Println("required", requiredParams)
-		for _, param := range fn.Type.Params.List {
-			var n = param.Names[0].Name
-			var t = param.Type
-			// check if exists in url
-			var inUrl = strings.Contains(httpMethod.Url, "{"+n+"}")
-			typeAsString := fmt.Sprintf("%v", t)
-			var method = typeToMethodMap[typeAsString]
-			var required = "false"
-			for _, item := range requiredParams {
-				if item == n {
-					required = "true"
+				var respToAdd = decoratorparser.StringifyResponse(decoratorparser.ResponseEntry{
+					StatusCode: 200,
+					Model:      returns.Prefix + "" + returns.ImportedPackage + "." + returns.Type,
+				})
+				autogenComments = append(autogenComments, respToAdd)
+			}
+			for _, param := range fn.Type.Params.List {
+				var n = param.Names[0].Name
+				var t = param.Type
+				// check if exists in url
+				var inUrl = strings.Contains(httpMethod.Url, "{"+n+"}")
+				typeAsString := fmt.Sprintf("%v", t)
+				var method = typeToMethodMap[typeAsString]
+				var required = "false"
+				for _, item := range requiredParams {
+					if item == n {
+						required = "true"
+					}
 				}
+				var pType = "query"
+				if inUrl {
+					pType = "path"
+				}
+				parametersList = append(parametersList, `NewArgumentParser(c, "`+pType+`", "`+n+`", `+required+`).`+method+`()`)
+				autogenComments = append(autogenComments, `@Param `+n+` `+pType+` `+typeAsString+` `+required+` "The `+n+`"`)
 			}
-			var pType = "query"
-			if inUrl {
-				pType = "path"
+			for i := range autogenComments {
+				autogenComments[i] = "// " + autogenComments[i]
 			}
-			parametersList = append(parametersList, `NewArgumentParser(c, "`+pType+`", "`+n+`", `+required+`).`+method+`()`)
-			autogenComments = append(autogenComments, `@Param `+n+` `+pType+` `+typeAsString+` `+required+` "The `+n+`"`)
-		}
-		for i := range autogenComments {
-			autogenComments[i] = "// " + autogenComments[i]
-		}
-		var functionName = functionNameRegex.ReplaceAllString(httpMethod.Url, "")
-		functionName = httpMethod.HttpMethod + "_" + functionName
-		for numberRegex.MatchString(functionName[0:1]) {
-			functionName = functionName[1:]
-		}
-		functionName = strings.ToLower(functionName)
-		if len(functionName) == 0 {
-			panic(errors.New("bad function name"))
-		}
-		var template = strings.Join(autogenComments, "\n") + `
+			var functionName = functionNameRegex.ReplaceAllString(httpMethod.Url, "")
+			functionName = httpMethod.HttpMethod + "_" + functionName
+			for numberRegex.MatchString(functionName[0:1]) {
+				functionName = functionName[1:]
+			}
+			functionName = strings.ToLower(functionName)
+			if len(functionName) == 0 {
+				panic(errors.New("bad function name"))
+			}
+			var methodCall = fn.Name.Name + `(` + strings.Join(parametersList, ",") + `)`
+			if apiController != nil {
+				methodCall = "New" + apiControllerName + "()." + methodCall
+			}
+			methodCall = packageName + "." + methodCall
+			var template = strings.Join(autogenComments, "\n") + `
 func ` + functionName + `(app *fiber.App) {
 	app.` + httpMethod.HttpMethod + `("` + httpMethod.Url + `", func (c *fiber.Ctx) error {
-		var result = ` + packageName + "." + fn.Name.Name + `(` + strings.Join(parametersList, ",") + `)
+		var result = ` + methodCall + `
 		return c.JSON(result)
 	})
 }`
-		controllerMethodDefinitiions = append(controllerMethodDefinitiions, template)
-		controllerMethods = append(controllerMethods, functionName)
+			controllerMethodDefinitions = append(controllerMethodDefinitions, template)
+			controllerMethods = append(controllerMethods, functionName)
+		}
 	}
 
 	var autogenFile = fileHeader
@@ -177,7 +233,7 @@ func ` + functionName + `(app *fiber.App) {
 		regStr += str + "(app)\n"
 	}
 	var defStr = ""
-	for _, str := range controllerMethodDefinitiions {
+	for _, str := range controllerMethodDefinitions {
 		defStr += str + "\n"
 	}
 	// templates
