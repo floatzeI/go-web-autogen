19933e664231310382733189b663ee70a121ab7c
beak <27046219+floatzeI@users.noreply.github.com>
2021-09-29 14:18:57 -0400

    add ResolveModelFolder

--- main.go
+++ main.go
@@ -1,10 +1,15 @@
 package main
 
-import "web-autogen/generator"
+import (
+	"web-autogen/generator"
+)
 
 func main() {
 	generator.Generate(generator.GenerateOptions{
 		Directory:         "./examples/project-one/",
 		ControllersFolder: "controllers",
+		ResolveModelFolder: func(packageName string, fileName string) string {
+			return "models/" + packageName
+		},
 	})
 }
